052c08eaf33bc428988d48929e7d8dde1e1e1ef0
Samriddhakc <5086samriddhakc@gmail.com>
2020-02-02 21:43:07 -0500

    week_2_changes

--- app/src/main/java/com/codepath/skc/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/codepath/skc/flixster/adapters/MovieAdapter.java
@@ -1,18 +1,25 @@
 package com.codepath.skc.flixster.adapters;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.content.Context;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.codepath.skc.flixster.DetailActivity;
 import com.codepath.skc.flixster.R;
 import com.codepath.skc.flixster.models.Movie;
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -58,7 +65,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
 
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-
+        RelativeLayout container;
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
@@ -68,12 +75,32 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
             tvTitle=itemView.findViewById(R.id.tvTitle);
             tvOverview=itemView.findViewById(R.id.tvOverview);
             ivPoster=itemView.findViewById(R.id.ivPoster);
+            container=itemView.findViewById(R.id.container);
         }
-
-        public void bind(Movie movie) {
+        
+        public void bind(final Movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
-            Glide.with(context).load(movie.getPosterPath()).into(ivPoster);
+            String imageUrl;
+            if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE)  {
+                imageUrl=movie.getBackdropPath();
+
+            }
+            else
+            {
+                imageUrl=movie.getPosterPath();
+            }
+
+            Glide.with(context).load(imageUrl).into(ivPoster);
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i=new Intent(context, DetailActivity.class);
+                    i.putExtra("movie",Parcels.wrap(movie));
+                    context.startActivity(i);
+
+                }
+            });
         }
     }
 }
--- app/src/main/java/com/codepath/skc/flixster/models/Movie.java
+++ app/src/main/java/com/codepath/skc/flixster/models/Movie.java
@@ -1,22 +1,35 @@
 package com.codepath.skc.flixster.models;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+        import org.json.JSONArray;
+        import org.json.JSONException;
+        import org.json.JSONObject;
+        import org.parceler.Parcel;
 
-import java.util.ArrayList;
-import  java.util.List;
+        import java.util.ArrayList;
+        import  java.util.List;
+
+@Parcel
 
 public class Movie {
 
+    int movieId;
+    String backdropPath;
     String posterPath;
     String title;
     String overview;
+    Double rating;
+
+    //empty constructor for the library
+
+    public Movie() {}
 
     public Movie(JSONObject jsonObject) throws JSONException {
+        backdropPath=jsonObject.getString("backdrop_path");
         posterPath=jsonObject.getString("poster_path");
         title=jsonObject.getString("title");
         overview=jsonObject.getString("overview");
+        rating=jsonObject.getDouble("vote_average");
+        movieId=jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJSONArray) throws JSONException {
@@ -31,11 +44,24 @@ public class Movie {
         return String.format("https://image.tmdb.org/t/p/w342/%s",posterPath);
     }
 
+    public String getBackdropPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", backdropPath);
+    }
+
     public String getTitle() {
+
         return title;
     }
 
     public String getOverview() {
         return overview;
+    }
+
+    public Float getRating() {
+        return Float.valueOf(String.valueOf(rating));
+    }
+
+    public int getMovieId() {
+        return movieId;
     }
 }
